710c6587c394dd28574b3de6c5cad3c65c296c11
Idanbenaim <idanbenaim@gmail.com>
2023-03-06 23:48:36 +0200

    images loading good

--- myproj/settings.py
+++ myproj/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+APPEND_SLASH=False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -124,6 +124,7 @@ STATIC_URL = "static/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'media'),]
+
 
 # STATIC_URL = 'static/'
 # MEDIA_URL = '/media/'
